6d7e807a5960be76052278c0e5595fda2d7b7482
JohannesDienst-askui <johannes.dienst@askui.com>
2022-10-07 10:14:30 +0200

    cypresss examples for xpath and css selectors

---
+++ cypress/cypress/e2e/spec.cy.js
@@ -0,0 +1,13 @@
+describe('xpath and css selector example spec', () => {
+  it('finds login button with xpath and css selectors', () => {
+    cy.visit('../minimalExamplePage.html')
+
+    cy.xpath('//button').should('be.visible');
+    cy.xpath('//*[@id="loginButton"]').should('be.visible');
+    cy.xpath('/html/body/div/button').should('be.visible');
+    
+    cy.get('#loginButton').should('be.visible');
+    cy.get('.login').should('be.visible');
+    cy.get('button').should('be.visible');
+  })
+})
