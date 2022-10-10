d59e523b088055d5e1ad4f1d04bb4402575b0cb7
JohannesDienst-askui <johannes.dienst@askui.com>
2022-10-10 09:24:21 +0200

    Add semicolon

--- cypress/cypress/e2e/spec.cy.js
+++ cypress/cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('xpath and css selector example spec', () => {
   it('finds login button with xpath and css selectors', () => {
-    cy.visit('../minimalExamplePage.html')
+    cy.visit('../minimalExamplePage.html');
 
     cy.xpath('//button').should('be.visible');
     cy.xpath('//*[@id="loginButton"]').should('be.visible');
